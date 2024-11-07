9eaa057b26f975a68e770130d0ae5a2d1cbda5ee
MarkusIngerslev <markusolsen2004@gmail.com>
2024-11-07 12:01:11 +0100

    Tilføjet at man kan trække i teskt på skærmen og får log'et ændring i console

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  PanGestureHandler,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
 
 export default function App() {
+  function onGestureEvent(event) {
+    const { translationX, translationY } = event.nativeEvent;
+    console.log("X: ", translationX);
+    console.log("Y: ", translationY);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={styles.rootView}>
+      <View style={styles.container}>
+        <PanGestureHandler onGestureEvent={onGestureEvent}>
+          <Text>Drag and watch console log</Text>
+        </PanGestureHandler>
+        <StatusBar style="auto" />
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
+  rootView: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
